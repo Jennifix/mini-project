1c6ab1d9350d0c667c54ae66ed10df896f9c8f34
Jenni <christoffersson.jenni@gmail.com>
2019-09-04 13:03:33 +0200

    Added Salary Correlation

--- static/js/graph.js
+++ static/js/graph.js
@@ -7,12 +7,22 @@ function makeGraphs(error, salaryData) {
 
     salaryData.forEach(function(d) {
         d.salary = parseInt(d.salary);
+        d.yrs_since_phd = parseInt(d["yrs.since.phd"]);
+        d.yrs_service = parseInt(d["yrs.service"]);
     })
 
     show_discipline_selector(ndx);
+
+    show_percent_that_are_professors(ndx, "Female", "#percentage-of-women-professors");
+    show_percent_that_are_professors(ndx, "Male", "#percentage-of-men-professors");
+    
     show_gender_balance(ndx);
     show_average_salary(ndx);
     show_rank_distribution(ndx);
+
+    show_service_to_salary_correlation(ndx);
+
+    show_phd_to_salary_correlation(ndx);
 
     dc.renderAll();
 }
@@ -24,6 +34,43 @@ function show_discipline_selector(ndx) {
     dc.selectMenu("#discipline-selector")
         .dimension(dim)
         .group(group);
+}
+
+function show_percent_that_are_professors(ndx, gender, element) {
+    var percentageThatAreProf = ndx.groupAll().reduce(
+        function (p, v) {
+            if (v.sex === gender) {
+                p.count++;
+                if (v.rank === "Prof") {
+                    p.are_prof++;
+                }
+            }
+            return p;
+        },
+        function (p, v) {
+            if (v.sex === gender) {
+                p.count--;
+                if (v.rank === "Prof") {
+                    p.are_prof--;
+                }
+            }
+            return p;
+        },
+        function () {
+            return {count: 0, are_prof: 0};
+        }
+    );
+
+    dc.numberDisplay(element)
+        .formatNumber(d3.format(".2%"))
+        .valueAccessor(function (d) {
+            if (d.count == 0) {
+                return 0;
+            } else {
+                return (d.are_prof / d.count);
+            }
+        })
+        .group(percentageThatAreProf);
 }
 
 function show_gender_balance(ndx) {
@@ -141,4 +188,76 @@ function show_rank_distribution(ndx) {
         
 
     console.log(profByGender.all());
+}
+
+function show_service_to_salary_correlation(ndx) {
+
+    var genderColors = d3.scale.ordinal()
+        .domain(["Female", "Male"])
+        .range(["pink", "blue"]);
+
+    var eDim = ndx.dimension(dc.pluck("yrs_service"));
+    var experienceDim = ndx.dimension(function(d) {
+        return [d.yrs_service, d.salary, d.rank, d.sex];
+    });
+    var experienceSalaryGroup = experienceDim.group();
+
+    var minExperience = eDim.bottom(1)[0].yrs_service;
+    var maxExperience = eDim.top(1)[0].yrs_service;
+
+    dc.scatterPlot("#service-salary")
+        .width(800)
+        .height(400)
+        .x(d3.scale.linear().domain([minExperience, maxExperience]))
+        .brushOn(false)
+        .symbolSize(8)
+        .clipPadding(10)
+        .xAxisLabel("Years Of Service")
+        .yAxisLabel("Salary")
+        .title(function (d) {
+            return d.key[2] + " earned " + d.key[1];
+        })
+        .colorAccessor(function (d) {
+            return d.key[3];
+        })
+        .colors(genderColors)
+        .dimension(experienceDim)
+        .group(experienceSalaryGroup)
+        .margins({top: 10, right: 50, bottom: 75, left: 75});
+}
+
+function show_phd_to_salary_correlation(ndx) {
+
+    var genderColors = d3.scale.ordinal()
+        .domain(["Female", "Male"])
+        .range(["pink", "blue"]);
+
+    var pDim = ndx.dimension(dc.pluck("yrs_since_phd"));
+    var phdDim = ndx.dimension(function (d) {
+        return [d.yrs_since_phd, d.salary, d.rank, d.sex];
+    });
+    var phdSalaryGroup = phdDim.group();
+
+    var minExperience = pDim.bottom(1)[0].yrs_since_phd;
+    var maxExperience = pDim.top(1)[0].yrs_since_phd;
+
+    dc.scatterPlot("#phd-salary")
+        .width(800)
+        .height(400)
+        .x(d3.scale.linear().domain([minExperience, maxExperience]))
+        .brushOn(false)
+        .symbolSize(8)
+        .clipPadding(10)
+        .xAxisLabel("Years Since PhD")
+        .yAxisLabel("Salary")
+        .title(function (d) {
+            return d.key[2] + " earned " + d.key[1];
+        })
+        .colorAccessor(function (d) {
+            return d.key[3];
+        })
+        .colors(genderColors)
+        .dimension(phdDim)
+        .group(phdSalaryGroup)
+        .margins({top: 10, right: 50, bottom: 75, left: 75});
 }
